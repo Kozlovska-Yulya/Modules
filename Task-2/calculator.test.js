5e43638788f284f07a38a9a9e5725122e09d132b
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2022-12-01 15:27:30 +0100

    corrected name of first function

--- Task-2/calculator.test.js
+++ Task-2/calculator.test.js
@@ -1,7 +1,7 @@
-import getSum, { getSquaredNumbers, getOddNumbers } from './calculator.js';
+import getSum, { getSquaredArray, getOddNumbers } from './calculator.js';
 
 it('should get squared numbers', () => {
-  const result = getSquaredNumbers([2, 3, 4, 5]);
+  const result = getSquaredArray([2, 3, 4, 5]);
   expect(result).toEqual([4, 9, 16, 25]);
 });
 
