4e913255dcf29f0dd773b6dd6a33d238387af81f
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2022-12-07 08:41:03 +0100

    test function withdraw

--- Task-5/task2/withdraw.test.js
+++ Task-5/task2/withdraw.test.js
@@ -1,20 +1,25 @@
 import { withdraw } from './withdraw.js';
 
 it('should find index of client', () => {
-  let personIndex = withdraw(['Kate', 'Yuliia', 'Igor', 'Sasha']);
+  let personIndex = withdraw(
+    ['Kate', 'Yuliia', 'Igor', 'Sasha'],
+    [100, 35, 80, 12],
+    'Igor',
+    10
+  );
   expect(personIndex).toEqual('Igor');
 });
 
-it('should balance >= amount ', () => {
-  let balance = withdraw([100, 35, 80, 12]);
-  expect(balance).toEqual([80]);
-});
+// it('should balance >= amount ', () => {
+//   let balances = withdraw([100, 35, 80, 12]);
+//   expect(balances).toEqual([80]);
+// });
 
-it('should calculate the remaining balance', () => {
-  expect(withdraw(80 - 10)).toEqual(70);
-});
+// it('should calculate the remaining balance', () => {
+//   expect(withdraw(80 - 10)).toEqual(70);
+// });
 
-it('should check if balance < amount ', () => {
-  let balance = withdraw([100, 35, 80, 12]);
-  expect(balance).toEqual(-1);
-});
+// it('should check if balance < amount ', () => {
+//   let balance = withdraw([100, 35, 80, 12]);
+//   expect(balance).toEqual(-1);
+// });
