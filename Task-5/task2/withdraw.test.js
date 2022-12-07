2fa5c439ae5c147d317dc2331e71833b55a9efb6
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2022-12-07 12:56:21 +0100

    tested if client can withdraw money from the balance

--- Task-5/task2/withdraw.test.js
+++ Task-5/task2/withdraw.test.js
@@ -20,11 +20,12 @@ it('should check if balance < amount ', () => {
   expect(balance).toEqual(-1);
 });
 
-// it('should balance >= amount ', () => {
-//   let balances = withdraw([100, 35, 80, 12]);
-//   expect(balances).toEqual([80]);
-// });
-
-// it('should calculate the remaining balance', () => {
-//   expect(withdraw(80 - 10)).toEqual(70);
-// });
+it('should check if contain function withdraw ', () => {
+  let result = withdraw(
+    ['Kate', 'Yuliia', 'Igor', 'Sasha'],
+    [100, 35, 60, 12],
+    'Igor',
+    10
+  );
+  expect(result).toBeDefined();
+});
